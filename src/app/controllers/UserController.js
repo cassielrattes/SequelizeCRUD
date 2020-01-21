c96c2ced2144eec29fd559b10f2c3ec5bdaa3ecd
cassielrattes <cassielrattes@gmail.com>
2020-01-21 00:40:06 -0300

    ...

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -23,9 +23,13 @@ class UserController {
 
   async store(req, res) {
     try {
-      const user = await User.create(req.body);
-
-      return res.json(user);
+      const user = await User.findOne({where: { email: req.body.email } })
+      
+      if(!user){
+      await User.create(req.body);
+      }
+      
+      return res.json(req.body);
     } catch (err) {
       return res.status(400).json({ error: err.message });
     }
